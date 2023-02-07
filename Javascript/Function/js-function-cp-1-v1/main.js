9eb5d7b79e7e85083aa2105a624ece61e272c0b4
daf2a <muhammaddaffaashdaqfillah@gmail.com>
2023-02-07 21:19:18 +0700

    complete assignment function js

--- Javascript/Function/js-function-cp-1-v1/main.js
+++ Javascript/Function/js-function-cp-1-v1/main.js
@@ -1,1 +1,21 @@
-// TODO: answer here
+function checkDatatype(a) {
+  if (typeof a === "string") {
+    return "string";
+  } else if (typeof a === "number") {
+    return "number";
+  } else if (typeof a === "boolean") {
+    return "boolean";
+  } else if (typeof a === "object") {
+    if (a === null) {
+      return "null";
+    } else if (Array.isArray(a)) {
+      return "array";
+    } else {
+      return "object";
+    }
+  } else if (typeof a === "function") {
+    return "function";
+  }
+}
+
+exports.checkDatatype = checkDatatype;
