5ec71aef8accff5e786f529529cb958551d8bab7
daf2a <muhammaddaffaashdaqfillah@gmail.com>
2023-02-07 11:37:42 +0700

    complete assignment iteration 1

--- Javascript/Iteratiaon/js-iteration-cp-1-v1/main.js
+++ Javascript/Iteratiaon/js-iteration-cp-1-v1/main.js
@@ -1,11 +1,17 @@
 function reverseUnique(word) {
-  return null // TODO: replace this
+  let res = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    if (word[i] !== word[i - 1]) {
+      res += word[i];
+    }
+  }
+  return res;
 }
 
-console.log(reverseUnique('greating')); //gnitaerg
-console.log(reverseUnique('learning')); //gninrael
-console.log(reverseUnique('book')); // kob
-console.log(reverseUnique('RuangGuru')); //uruGnauR
-console.log(reverseUnique('I am reading a book how to hunt a deer')); // red a tnuh ot woh kob a gnidaer ma I
+console.log(reverseUnique("greating")); //gnitaerg
+console.log(reverseUnique("learning")); //gninrael
+console.log(reverseUnique("book")); // kob
+console.log(reverseUnique("RuangGuru")); //uruGnauR
+console.log(reverseUnique("I am reading a book how to hunt a deer")); // red a tnuh ot woh kob a gnidaer ma I
 
 module.exports = reverseUnique;
